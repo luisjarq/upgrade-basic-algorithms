84671cfe77bec3dc1710713e3ea82fa3461c0b04
Luisjarq <lc.luisja@gmail.com>
2021-10-19 22:40:55 +0200

    Iteration#2

--- BasicAlgorithms.js
+++ BasicAlgorithms.js
@@ -1,5 +1,32 @@
+// Iteración #1
+    // 1.1
 let myFavoriteHero = "Hulk";
+    // 1.2
 let x = 50;
 let h = 5;
+    //1.2
 let y = 10;
+    // 1.4
 let z = h + y;
+// Iteración #2
+    // 1.1
+const character = { name: "Jack Sparrow", age: 10 };
+character.age = 25;
+    // 1.2
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+console.log(
+  `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`
+);
+    // 1.3
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
+console.log(toy1.price + toy2.price);
+    // 1.4
+let globalBasePrice = 10000;
+const car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
+const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
